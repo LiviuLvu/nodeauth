a021172d6b41100b5126068a6cb2d7f0c5c96aff
Liviu Iancu <liviuiancu@gmx.com>
2017-04-09 19:13:58 +0300

    rendering message confirmation after user register

--- routes/users.js
+++ routes/users.js
@@ -62,6 +62,8 @@ router.post('/register', upload.single('profileimage'), function(req, res, next)
       }
       console.log(user);
 
+      req.flash('success', 'You are now registered and can login');
+
       res.location('/');
       res.redirect('/');
     });
